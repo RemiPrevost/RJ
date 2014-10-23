b0010a43504c21dfc3b386a7138c3ae9cae1963f
PierreAlexMaury <maurypalam@hotmail.fr>
2014-10-23 17:56:00 -0200

    Version1.2

--- RioSport+/app/src/androidTest/java/com/google/riosport/ApplicationTest.java
+++ RioSport+/app/src/androidTest/java/com/google/riosport/ApplicationTest.java
@@ -1,4 +1,4 @@
-package rio.riosport;
+package com.google.riosport;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- RioSport+/app/src/main/java/com/google/riosport/customTextView.java
+++ RioSport+/app/src/main/java/com/google/riosport/customTextView.java
@@ -1,4 +1,4 @@
-package rio.riosport;
+package com.google.riosport;
 
 import android.content.Context;
 import android.graphics.Typeface;
